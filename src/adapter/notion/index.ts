5d1dd9e8ac3135c77601b2558f039afbbe2c078f
Choharim <jhl8586@naver.com>
2025-02-09 18:18:54 +0900

    refactor: notionAPI 필요한 함수 추가

--- src/adapter/notion/index.ts
+++ src/adapter/notion/index.ts
@@ -13,6 +13,7 @@ class NotionAPI {
 
   private BASE_URL = 'https://notion-api.splitbee.io/v1'
 
+  // 모든 FrontMatter을 가져옵니다.
   private async getPostFrontMatters(): Promise<PostFrontMatter[]> {
     return await fetch(`${this.BASE_URL}/table/${this.NOTION_ID.page.blog}`, {
       headers: {
@@ -21,27 +22,51 @@ class NotionAPI {
       },
     }).then((res) => res.json())
   }
+
+  // 게시된 모든 FrontMatter을 가져옵니다.
   public async getPublishedPostFrontMatters(): Promise<PostFrontMatter[]> {
     const all = await this.getPostFrontMatters()
 
     return PostEntity.filterPublishedFrontMatters(all)
   }
 
+  // 추천된 모든 FrontMatter을 가져옵니다.
   public async getRecommandPostFrontMatters(): Promise<PostFrontMatter[]> {
     const all = await this.getPostFrontMatters()
 
     return PostEntity.filterRecommandFrontMatters(all)
   }
 
+  // 특정 category의 모든 FrontMatter을 가져옵니다.
+  public async getFrontMattersByCategory(
+    category: PostCategory
+  ): Promise<PostFrontMatter[]> {
+    const all = await notionAPI.getPublishedPostFrontMatters()
+    const categorized = PostEntity.filterFrontMattersByCategory(all, category)
+
+    return categorized
+  }
+
+  // 특정 id의 FrontMatter을 가져옵니다.
+  public async getFrontMatterById(
+    id: string
+  ): Promise<PostFrontMatter | undefined> {
+    const all = await this.getPublishedPostFrontMatters()
+    const frontMatter = PostEntity.findFrontMatter(all, id)
+
+    return frontMatter
+  }
+
+  // 특정 페이지를 가져옵니다.
   public async getPost(id: string) {
     return await notionClient.getPage(id)
   }
 
-  public async getTable() {
+  private async getTable() {
     return await notionClient.getPage(this.NOTION_ID.page.blog)
   }
 
-  public async getFilters() {
+  private async getFilters() {
     const tableOfPosts = await this.getTable()
     const filterCodes: string[] =
       Object.values(
@@ -70,6 +95,7 @@ class NotionAPI {
     return filters as { category: PostCategory[] }
   }
 
+  // 모든 카데고리를 가져옵니다.
   public async getCategories(): Promise<PostCategory[]> {
     const filtes = await this.getFilters()
 
